00bf7a5f6dc97da83e24cca3bd192547e65405c8
bedinuratascan <bedinuratascan@gmail.com>
2020-04-07 22:17:44 +0300

    13th-Video/Added-Contact-Form

--- home/models.py
+++ home/models.py
@@ -2,6 +2,8 @@ from ckeditor_uploader.fields import RichTextUploadingField
 from django.db import models
 
 # Create your models here.
+from django.forms import ModelForm, TextInput, Textarea
+
 
 class Setting(models.Model):
     STATUS = (
@@ -34,6 +36,33 @@ class Setting(models.Model):
     def __str__(self):
         return self.title
 
+class ContactFormMessage(models.Model):
+    STATUS = (
+        ('New', 'New'),
+        ('Read', 'Read'),
+        ('Closed', 'Closed'),
+    )
+    name = models.CharField(blank=True, max_length=70)
+    email = models.CharField(blank=True, max_length=50)
+    subject = models.CharField(blank=True, max_length=50)
+    message = models.CharField(blank=True, max_length=255)
+    status = models.CharField(max_length=10, choices=STATUS, default='New')
+    ip = models.CharField(blank=True, max_length=20)
+    note = models.CharField(blank=True, max_length=100)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.name
 
+class ContactFormu(ModelForm):
+    class Meta:
+        model = ContactFormMessage
+        fields = ['name', 'email', 'subject', 'message']
+        widgets = {
+            'name': TextInput(attrs={'class': 'controls', 'placeholder': 'Full Name'}),
+            'email': TextInput(attrs={'class': 'controls', 'placeholder': 'Email Address'}),
+            'subject': TextInput(attrs={'class': 'controls', 'placeholder': 'Subject'}),
+            'message': Textarea(attrs={'class': 'controls', 'placeholder': 'Message'}),
+        }
 
