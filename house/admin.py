ee384b13439d51983792e19e818a2a1aa5724693
bedinuratascan <bedinuratascan@gmail.com>
2020-04-11 21:55:09 +0300

    15th-Video/Added-Category-And-Subcategory-Menu

--- house/admin.py
+++ house/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from mptt.admin import MPTTModelAdmin, DraggableMPTTAdmin
+
 from house.models import Category, House, Images
 
 class HouseImageInline(admin.TabularInline):
@@ -8,22 +10,53 @@ class HouseImageInline(admin.TabularInline):
     extra = 6
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['title', 'image_tag', 'status']
+    list_display = ['title', 'status']
     list_filter = ['status']
-    readonly_fields = ('image_tag',)
-
 
 class HouseAdmin(admin.ModelAdmin):
     list_display = ['title', 'category', 'rent', 'image_tag', 'status']
+    readonly_fields = ('image_tag',)
     list_filter = ['status', 'category']
     inlines = [HouseImageInline]
-    readonly_fields = ('image_tag',)
 
 class ImagesAdmin(admin.ModelAdmin):
     list_display = ['title', 'house', 'image_tag']
     readonly_fields = ('image_tag',)
 
+class CategoryAdmin2(DraggableMPTTAdmin):
+    mptt_indent_field = "title"
+    list_display = ('tree_actions', 'indented_title',
+                    'related_houses_count', 'related_houses_cumulative_count')
+    list_display_links = ('indented_title',)
 
-admin.site.register(Category, CategoryAdmin)
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+
+        # Add cumulative houses count
+        qs = Category.objects.add_related_count(
+                qs,
+                House,
+                'category',
+                'houses_cumulative_count',
+                cumulative=True)
+
+        # Add non cumulative houses count
+        qs = Category.objects.add_related_count(qs,
+                 House,
+                 'category',
+                 'houses_count',
+                 cumulative=False)
+        return qs
+
+    def related_houses_count(self, instance):
+        return instance.houses_count
+    related_houses_count.short_description = 'Related houses (for this specific category)'
+
+    def related_houses_cumulative_count(self, instance):
+        return instance.houses_cumulative_count
+    related_houses_cumulative_count.short_description = 'Related houses (in tree)'
+
+
+admin.site.register(Category, CategoryAdmin2)
 admin.site.register(House, HouseAdmin)
 admin.site.register(Images, ImagesAdmin)
