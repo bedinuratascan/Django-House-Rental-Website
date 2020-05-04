8140377a3da590fc5ce15ff91a67e363c88411a6
bedinuratascan <bedinuratascan@gmail.com>
2020-05-04 18:34:02 +0300

    24th-Video/Created-User-Signup-Form

--- home/views.py
+++ home/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from home.forms import SearchForm
+from home.forms import SearchForm, SignUpForm
 from home.models import Setting, ContactFormMessage, ContactFormu
 from house.models import House, Category, Images, Comment
 
@@ -117,6 +117,25 @@ def login_view(request):
     category = Category.objects.all()
     context = {'category': category}
     return render(request, 'login.html', context)
+
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(request, username=username, password=password)
+            login(request, user)
+            return HttpResponseRedirect('/')
+
+    form = SignUpForm()
+    category = Category.objects.all()
+    context = {'category': category,
+               'form': form}
+    return render(request, 'signup.html', context)
+
 
 
 
